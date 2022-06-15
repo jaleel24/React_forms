fdb1554b2e0a4aefcdeca0bcd806bf4ab2731676
jaleel24 <36284471+jaleel24@users.noreply.github.com>
2022-06-15 18:54:26 -0400

    added validations for email and validated the overall form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -19,23 +19,47 @@ const BasicForm = (props) => {
     InputBlurHandler: LNameBlurHandler,
     reset: LnameReset,
   } = useInput((value) => value.trim() !== "");
+//! custome hook use for Email
+const {
+  value: email,
+  hasError: emailHasError,
+  isValid: emailIsValid,
+  valueHandler: emailHandler,
+  InputBlurHandler: emailBlurHandler,
+  reset: emailReset,
+} = useInput((value) => value.trim() !== "" && value.includes('@'));
 
   //! form Handler Function
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
-    if (!nameIsValid && nameHasError ) {
-      return;
-    }
-    if(lNameIsValid && lNameHasError){
+    // if (!nameIsValid && nameHasError ) {
+    //   return;
+    // }
+    // if(!lNameIsValid && lNameHasError){
+    //   return
+    // }
+    // if(!emailIsValid && emailHasError){
+    //   return
+    // }
+    if(!formIsValid){
       return
     }
-
+    console.log('Submitted');
+    console.log(name,lName,email);
     nameReset();
     LnameReset();
+    emailReset();
   };
 
+  let formIsValid = false;
+
+  if (nameIsValid && lNameIsValid && emailIsValid){
+    formIsValid = true;
+  }
     let nameClassHandler = nameHasError ? 'form-control invalid' : 'form-control';
+    let lnameClassHandler = lNameHasError ? 'form-control invalid' : 'form-control';
+    let emailClassHandler = emailHasError ? 'form-control invalid' : 'form-control';
  
  
     return (
@@ -45,21 +69,24 @@ const BasicForm = (props) => {
         <div className={nameClassHandler}>
           <label htmlFor="name">First Name</label>
           <input type="text" id="name" value={name} onChange={nameHandler} onBlur={nameBlurHandler} />
+          {nameHasError && <p className="error-text">please enter a valid name</p>}
         </div>
-        {nameHasError && <p className="error-text">please enter a valid name</p>}
+       
         
-        <div className="form-control">
+        <div className={lnameClassHandler}>
           <label htmlFor="name">Last Name</label>
           <input type="text" id="name" value={lName} onChange={LNameHandler} onBlur={LNameBlurHandler}/>
+          {lNameHasError && <p className="error-text">please enter a valid Last name</p>}
         </div>
-        {lNameHasError && <p className="error-text">please enter a valid Last name</p>}
+      
       </div>
-      <div className="form-control">
+      <div className={emailClassHandler}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="email" id="name" />
+        <input type="email" id="name"  value={email} onChange={emailHandler} onBlur={emailBlurHandler}/>
+        {emailHasError && <p className="error-text">please enter a valid Last name</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
