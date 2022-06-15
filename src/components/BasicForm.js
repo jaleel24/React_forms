071e5c4c15c7d4c1ad7aca9e41567a6139b7d54c
jaleel24 <36284471+jaleel24@users.noreply.github.com>
2022-06-15 15:38:46 -0400

    added a new form and added validatons for first and lastName using customhook

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,64 @@
+import useInput from "../hooks/use-input";
+
+//! custome hook use for firsname
 const BasicForm = (props) => {
-  return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+  const {
+    value: name,
+    hasError: nameHasError,
+    isValid: nameIsValid,
+    valueHandler: nameHandler,
+    InputBlurHandler: nameBlurHandler,
+    reset: nameReset,
+  } = useInput((value) => value.trim() !== "");
+//! custome hook use for firsname
+  const {
+    value: lName,
+    hasError: lNameHasError,
+    isValid: lNameIsValid,
+    valueHandler: LNameHandler,
+    InputBlurHandler: LNameBlurHandler,
+    reset: LnameReset,
+  } = useInput((value) => value.trim() !== "");
+
+  //! form Handler Function
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!nameIsValid && nameHasError ) {
+      return;
+    }
+    if(lNameIsValid && lNameHasError){
+      return
+    }
+
+    nameReset();
+    LnameReset();
+  };
+
+    let nameClassHandler = nameHasError ? 'form-control invalid' : 'form-control';
+ 
+ 
+    return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="control-group">
+        
+        <div className={nameClassHandler}>
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" value={name} onChange={nameHandler} onBlur={nameBlurHandler} />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        {nameHasError && <p className="error-text">please enter a valid name</p>}
+        
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" value={lName} onChange={LNameHandler} onBlur={LNameBlurHandler}/>
         </div>
+        {lNameHasError && <p className="error-text">please enter a valid Last name</p>}
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="email" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,58 +1,53 @@
-import { useState } from "react";
+//import { useState } from "react";
 
 import useInput from "../hooks/use-input";
 const SimpleInput = (props) => {
   //? useInput is a custom hook so now we will pull out the values which it returns by destructuring
-
   const {
     value: enteredName,
-    isValid:nameIsValid,
+    isValid: nameIsValid,
     hasError: nameInputHasError,
     valueHandler: valueChangeHandler,
     InputBlurHandler: inputBlurHandler,
-    reset:resetData
+    reset: resetData,
   } = useInput((value) => value.trim() !== "");
+
+  // using the custom hook for the email
+
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailInputHasError,
+    valueHandler: emailChangeHandler,
+    InputBlurHandler: emailinputBlurHandler,
+    reset: resetEmail,
+  } = useInput((value) => value.trim() !== "" && value.includes("@"));
 
   //! storing/fetching data by using use state
   //! useState is better when u want to validate the user input on every key stroke
   //! it is better when u want to reset the input ,
   //! useState is better
-
-  const [enteredEmail, setEmail] = useState("");
-  const [EmailInputIsTouched, setEmailIsInputTouched] = useState(false);
-
-  const emailIsvalid = enteredEmail.trim() !== "" && enteredEmail.includes("@");
-  const emailInputIsInvalid = !emailIsvalid && EmailInputIsTouched;
-
   let formIsValid = false;
 
-  if (nameIsValid && emailIsvalid) {
+  if (nameIsValid && emailIsValid) {
     formIsValid = true;
   }
-  const emailHandler = (event) => {
-    setEmail(event.target.value);
-  };
-  const emailInputBlurHandler = () => {
-    setEmailIsInputTouched(true);
-  };
+
   const submitNameHandler = (event) => {
     event.preventDefault();
-
-    setEmailIsInputTouched(true);
     //!const enteredValue = NameinputRef.current.value;
     //!NameinputRef.current.value = ''  NOT IDEAL, DONT MANUPULATE THE DOM DIRECTLY, LEAVE IT TO REACT
-    if (!nameIsValid && !emailIsvalid) {
+    if (!nameIsValid && !emailIsValid) {
       return;
     }
     resetData();
-    setEmail("");
-    setEmailIsInputTouched(false);
+    resetEmail();
   };
 
   const classHandler = nameInputHasError
     ? "form-control invalid"
     : "form-control";
-  const emailclassHandler = emailInputIsInvalid
+  const emailclassHandler = emailInputHasError
     ? "form-control invalid"
     : "form-control";
   return (
@@ -80,11 +75,11 @@ const SimpleInput = (props) => {
           // ref={NameinputRef}
           type="email"
           id="email"
-          onChange={emailHandler}
-          onBlur={emailInputBlurHandler}
+          onChange={emailChangeHandler}
+          onBlur={emailinputBlurHandler}
           value={enteredEmail}
         />
-        {emailInputIsInvalid && (
+        {emailInputHasError && (
           <p className="error-text"> Please enter a Valid email </p>
         )}
       </div>
